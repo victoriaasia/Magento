b4bc7f559c6a2acbaeb1b9233baebf8f0e766634
V A <V A>
2016-07-09 17:59:09 +0300

    styles fixed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-$('a[href^="#"]').bind('click.smoothscroll',function (e) {
- e.preventDefault();
-
-var target = this.hash,
- $target = $(target);
-
-$('html, body').stop().animate({
- 'scrollTop': $target.offset().top
- }, 700, 'swing', function () {
- window.location.hash = target;
- });
- });
-
- $('.hamburger').on('click', function () {
+$('a[href^="#"]').bind('click.smoothscroll', function(e) {
+  e.preventDefault();
+  var target = this.hash,
+    $target = $(target);
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top
+  }, 700, 'swing', function() {
+    window.location.hash = target;
+  });
+});
+$('.hamburger').on('click', function() {
   var menu = $('.dropdown');
   menu.toggleClass('menu-active');
 });
