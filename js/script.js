a01f5a36f0b7174b9473a8f0ef35bad1841c4b58
V A <V A>
2016-07-09 17:55:49 +0300

    added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+
+var target = this.hash,
+ $target = $(target);
+
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 700, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+ $('.hamburger').on('click', function () {
+  var menu = $('.dropdown');
+  menu.toggleClass('menu-active');
+});
